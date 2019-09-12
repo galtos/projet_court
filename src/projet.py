fb4aae0d61974bb4c0c2ec919c255dfb715e227b
m2bi Guillaume OLLITRAULT <guillaume.ollitrault@gmail.com>
2019-09-12 17:39:11 +0200

    ebauche de projet avec lecture et transformation AS et test de class "statistique" ajout de fichiers de md test

--- src/projet.py
+++ src/projet.py
@@ -12,20 +12,37 @@ Creer classe avec soit :
 Dataframe :
 - liste de liste avec assignation de l'alphabet
 """
-input_topology, _ = [sys.argv[1],sys.argv[1]] #gro
-input_trajectory, _ = [sys.argv[2],sys.argv[2]] #xtc
+input_topology,_ = [sys.argv[1],sys.argv[1]] #gro
+input_trajectory,_ = [sys.argv[2],sys.argv[2]] #xtc
 print(input_topology, input_trajectory)
 
-class Structure_assign():
+class StructureAssign():
     def __init__(self, input_topology, input_trajectory):
-	    for chain_name, chain in pbx.chains_from_trajectory(input_trajectory, input_topology):
+        self.sa_md = []
+        
+        for chain_name, chain in pbx.chains_from_trajectory(input_trajectory, input_topology):
 	        dihedrals = chain.get_phi_psi_angles()
 	        pb_seq = pbx.assign(dihedrals)
-	        print('* {}'.format(chain_name))
-	        print('  {}'.format(pb_seq))
+	        self.sa_md.append(pb_seq)
+	        #print('* {}'.format(chain_name))
+	        #print('  {}'.format(pb_seq))
+
+class Statistique():
+    def __init__(self, sa_md):
+        self.sa_md = sa_md
+
+    def shannon_entropy(self):
+        print(len(self.sa_md),len(self.sa_md[0]))
+        #for i in range(len(self.sa_md[])):
+        
+        
+        
 
 #### MAIN ####
 
-dt = Structure_assign(input_topology, input_trajectory)
+dt = StructureAssign(input_topology, input_trajectory)
+print(dt.sa_md[1][1])
+stat = Statistique(dt.sa_md)
+stat.shannon_entropy()
 
 
