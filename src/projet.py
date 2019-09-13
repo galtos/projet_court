926b83419d93a49d8789b29044385bfab52ea4cc
guillaume <guillaume.ollitrault@gmail.com>
2019-09-13 15:35:07 +0200

    ajout de la fonction qui calcul la probilité de P(a) pour chaque frame

--- src/projet.py
+++ src/projet.py
@@ -2,6 +2,7 @@ from pprint import pprint
 import urllib.request
 import os
 import sys
+import numpy as np
 import pbxplore as pbx
 
 """
@@ -30,15 +31,37 @@ class StructureAssign():
 class Statistique():
     def __init__(self, sa_md):
         self.sa_md = sa_md
-    def matrice_proba_positions(self):
+        
+        
+    def matrice_p_a(self):
         """
         compte le nombre de lettre du SA dans la MD 
         """
         nSA = {"a":0,"b":0,"c":0,"d":0,"e":0,"f":0,"g":0,"h":0,"i":0,"j":0,"k":0,"l":0,"m":0,"n":0,"o":0,"p":0,"Z":0}
+        
+        nLetters = len(self.sa_md[0])
+        nFrames = len(self.sa_md)
+        mat_probabilities = []
+
         for i in range(len(self.sa_md)):
+            mat_probabilities.append([])
             for j in range(len(self.sa_md[0])):
                 nSA[self.sa_md[i][j]] +=1
-        print(nSA)    
+            for k in nSA:
+                if nSA[k] == 0:
+                    
+                    mat_probabilities[i].append(0)
+                else:
+                    mat_probabilities[i].append(nSA[k]/nLetters)   
+
+                nSA[k] = 0
+            
+
+
+        print(nSA)  
+        
+        
+          
     def mutual_information(self):
         print(len(self.sa_md),len(self.sa_md[0]))
         #for i in range(len(self.sa_md[])):
@@ -52,5 +75,5 @@ dt = StructureAssign(input_topology, input_trajectory)
 print(dt.sa_md[1][1])
 stat = Statistique(dt.sa_md)
 stat.mutual_information()
-stat.matrice_proba_positions()
+stat.matrice_p_a()
 
