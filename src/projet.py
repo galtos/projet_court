1416c58ccc9e1b0ff5fdba59e00f1d62d9008774
m2bi Guillaume OLLITRAULT <guillaume.ollitrault@gmail.com>
2019-09-17 14:17:15 +0200

    code rendu plus beau (nom variables etc...)  Veuillez saisir le message de validation pour vos modifications. Les lignes

--- src/projet.py
+++ src/projet.py
@@ -1,45 +1,43 @@
-import sys
 import os
+import argparse
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import argparse
 import pbxplore as pbx
 
 SA_SIZE = 17
 SA_LETTERS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",\
               "l", "m", "n", "o", "p", "Z"]
 
-#class Configuration
-
-class AssignationPbxplore():
+class AssignationPbxplore(object):
     """
     This class assign structural letters to a protein structure with pbxplore 
     to each frame of a molecular dynamics. The class takes 3 arguments:
-    
+
     - the topology of the structure
     - the trajectory of the molecular dynamics
     - the argument to know if a fasta output file is needed
-    
+
     the set of structural letters is stored in : md_sa_seq
     """
-    def __init__(self, input_topology, input_trajectory, args):
+    def __init__(self, args):
         self.md_sa_seq = []
         if args.f:
-            file_output_fasta = open(args.f,"w")
-        for chain_name, chain in pbx.chains_from_trajectory(input_trajectory,\
-                                                            input_topology):
-	        dihedrals = chain.get_phi_psi_angles()
-	        pb_seq = pbx.assign(dihedrals)
-	        self.md_sa_seq.append(pb_seq)
-	        if args.f:
-	            file_output_fasta.write(">{}\n".format(chain_name))
-	            file_output_fasta.write("{}\n".format(pb_seq))
+            file_output_fasta = open(args.f, "w")
+        for chain_name, chain in pbx.chains_from_trajectory(
+                                                        args.input_trajectory,\
+                                                        args.input_topology):
+            dihedrals = chain.get_phi_psi_angles()
+            pb_seq = pbx.assign(dihedrals)
+            self.md_sa_seq.append(pb_seq)
+            if args.f:
+                file_output_fasta.write(">{}\n".format(chain_name))
+                file_output_fasta.write("{}\n".format(pb_seq))
         if args.f:
             file_output_fasta.close()
 
-class Statistique():
+class Statistique(object):
     """
     This class contain the differents methods to perform the statistics 
     analysis of the set of structural letters of the structure during the 
@@ -50,8 +48,7 @@ class Statistique():
     """
     def __init__(self, md_sa_seq):
         self.md_sa_seq = md_sa_seq
-        
-       
+
     def matrice_p_a(self):
         """
         Method calculate the probability for a structural letter to occur in 
@@ -59,15 +56,14 @@ class Statistique():
         Return a matrix 1*SA_SIZE with the probability for each letters
         to occur.
         """
-        mat_probabilities = pd.DataFrame(np.zeros(SA_SIZE), index = SA_LETTERS)
+        mat_probabilities = pd.DataFrame(np.zeros(SA_SIZE), index=SA_LETTERS)
 
         for i in range(len(self.md_sa_seq)):
             for j in range(len(self.md_sa_seq[0])):
                 mat_probabilities[0][self.md_sa_seq[i][j]] += 1
                 
-        return(mat_probabilities/(len(self.md_sa_seq)*len(self.md_sa_seq[0])))
-        
-    
+        return mat_probabilities/(len(self.md_sa_seq)*len(self.md_sa_seq[0]))
+
     def matrice_p_ab(self):
         """
         Method calculate the probability for a two set of structural letters to 
@@ -82,68 +78,74 @@ class Statistique():
         Return a matrix of size SA_SIZE*SA_SIZE with the probability for each 
         pair of letters. P(a,b)
         """
-        mat_count_occurence = pd.DataFrame(                       \
-                                     np.zeros((SA_SIZE, SA_SIZE)),\
-                                     columns = SA_LETTERS, index = SA_LETTERS) 
+        mat_count_occurence = pd.DataFrame(                            \
+                                          np.zeros((SA_SIZE, SA_SIZE)),\
+                                          columns=SA_LETTERS, index=SA_LETTERS) 
 
         for i in range(len(self.md_sa_seq)-1):
             for j in range(len(self.md_sa_seq[0])):
                 mat_count_occurence[self.md_sa_seq[i][j]]\
                                    [self.md_sa_seq[i+1][j]] += 1
-        
+
         ####probabilité jointe ####
-        return(mat_count_occurence/\
-              (len(self.md_sa_seq)*len(self.md_sa_seq[0])))
+        return mat_count_occurence/(len(self.md_sa_seq)*len(self.md_sa_seq[0]))
+
     def mutual_information_i(self, i, j, mat_p_a, mat_p_ab):
         """
-        Compute the mutual information for two sets of positions in the structure
-        I(X;Y) =  ∑ P(x;y)*log(P(x;y)/(P(x)*P(y)))
+        Compute the mutual information for two sets of positions in the 
+        structure. MI(X;Y) =  ∑ P(x;y)*log(P(x;y)/(P(x)*P(y)))
         """
-        I = 0
+        mutual_information = 0
         for k in range(len(self.md_sa_seq)): 
-                if mat_p_ab[self.md_sa_seq[k][i]]\
-                           [self.md_sa_seq[k][j]] == 0 \
-                or mat_p_a[0][self.md_sa_seq[k][i]] == 0 \
-                or mat_p_a[0][self.md_sa_seq[k][j]] == 0:  
-                    I += 0
-                else:               
-                    I += mat_p_ab[self.md_sa_seq[k][i]][self.md_sa_seq[k][j]]*\
-                 np.log2(mat_p_ab[self.md_sa_seq[k][i]][self.md_sa_seq[k][j]]/\
-                (mat_p_a[0][self.md_sa_seq[k][i]]*\
-                 mat_p_a[0][self.md_sa_seq[k][j]]))
-    
-    
-        return I
+            if mat_p_ab[self.md_sa_seq[k][i]]\
+                       [self.md_sa_seq[k][j]] == 0 \
+            or mat_p_a[0][self.md_sa_seq[k][i]] == 0 \
+            or mat_p_a[0][self.md_sa_seq[k][j]] == 0:  
+                mutual_information += 0
+            else:               
+                mutual_information += \
+                    mat_p_ab[self.md_sa_seq[k][i]][self.md_sa_seq[k][j]]*\
+           np.log2(mat_p_ab[self.md_sa_seq[k][i]][self.md_sa_seq[k][j]]/\
+           (mat_p_a[0][self.md_sa_seq[k][i]]*mat_p_a[0][self.md_sa_seq[k][j]]))
+
+        return mutual_information
+
     def mutual_information(self):
         """
         Compute the mutual information for each pair of positions in the 
         structure.
-        return the matrix of size (number of position inf the structure)**2 with
-        each mutual information calculate.
+        return the matrix of size (number of position inf the structure)**2 
+        with each mutual information calculate.
         """
-        MI = pd.DataFrame(np.zeros((len(self.md_sa_seq[0]),\
+        mutual_information = pd.DataFrame(np.zeros((len(self.md_sa_seq[0]),\
                                     len(self.md_sa_seq[0]))))
-        
+
         mat_p_a = self.matrice_p_a()
         mat_p_ab = self.matrice_p_ab()
-        
+
         for i in range(len(self.md_sa_seq[0])):
             for j in range(0, len(self.md_sa_seq[0])):
-                MI[i][j] = self.mutual_information_i(i, j, mat_p_a, mat_p_ab)
-        return(MI)
-        
-class Visualisation():
-    def __init__(self, matrice_MI):
-        self.matrice_MI = matrice_MI
-        
+                mutual_information[i][j] = self.mutual_information_i(\
+                                                i, j, mat_p_a, mat_p_ab)
+        return mutual_information
+
+class Visualisation(object):
+    """
+    Contains the methods for the visualization of the mutual information matrix
+    with heatmap and a network representation with pymol. This class also allow
+    the user to write a cvs file with the mutual information matrix.
+    """
+    def __init__(self, matrice_mi):
+        self.matrice_mi = matrice_mi
+
     def write_mi_csv(self, args):
         """
         Write a csv file containing the utual inforamtion matrix
         """
-        file_csv = open(args.omi,"w")
-        for i in range(len(self.matrice_MI)):
-            for j in range(len(self.matrice_MI)):
-                file_csv.write("{},".format(self.matrice_MI[i][j]))
+        file_csv = open(args.omi, "w")
+        for i in range(len(self.matrice_mi)):
+            for j in range(len(self.matrice_mi)):
+                file_csv.write("{},".format(self.matrice_mi[i][j]))
             file_csv.write("\n")
         file_csv.close()       
     def visualize_matrice_mi(self):
@@ -151,8 +153,9 @@ class Visualisation():
         Represent the mutual information matrix in form of a heatmap
         """
         ax = plt.axes()  
-        sns.heatmap(self.matrice_MI, ax = ax)
-        ax.set_title("Heatmap of the compute mutual information for the different positions")
+        sns.heatmap(self.matrice_mi, ax=ax)
+        ax.set_title(\
+       "Heatmap of the compute mutual information for the different positions")
         plt.show()
 
     def color_palette(self):
@@ -161,86 +164,95 @@ class Visualisation():
         the important residues involved in the network.
         The colors goes from the blue to red.
         """
-        color =[[ 0.00,  0.00,  1.00],\
-                [ 0.24,  0.16,  0.75],\
-                [ 0.36,  0.24,  0.63],\
-                [ 0.53,  0.34,  0.47],\
-                [ 0.77,  0.50,  0.22],\
-                [ 1.00,  0.63,  0.00],\
-                [ 1.00,  0.50,  0.00],\
-                [ 1.00,  0.37,  0.00],\
-                [ 1.00,  0.24,  0.00],\
-                [ 1.00,  0.10,  0.00],\
-                [ 1.00,  0.00,  0.00]]
+        color = [[0.00, 0.00, 1.00],\
+                 [0.24, 0.16, 0.75],\
+                 [0.36, 0.24, 0.63],\
+                 [0.53, 0.34, 0.47],\
+                 [0.77, 0.50, 0.22],\
+                 [1.00, 0.63, 0.00],\
+                 [1.00, 0.50, 0.00],\
+                 [1.00, 0.37, 0.00],\
+                 [1.00, 0.24, 0.00],\
+                 [1.00, 0.10, 0.00],\
+                 [1.00, 0.00, 0.00]]
         return color
-        
-            
+  
     def show_pymol_network(self, input_pdb, line_threshold):
         """
-        launch pymol and read a pml file which color the résidues according their 
-        significance in the network made with the mutual information. Put a line between 
-        the important residues (>threshold).
-        the threshold is is the maximum value of the matrix divided by 2 but can be given by the user
+        launch pymol and read a pml file which color the résidues according 
+        their significance in the network made with the mutual information. 
+        Put a line between the important residues (>threshold).
+        the threshold is is the maximum value of the matrix divided by 2 but 
+        can be given by the user.
         """
-        input_pymol = open("pymol_tmp.pml","w")
+        input_pymol = open("pymol_tmp.pml", "w")
         input_pymol.write("load "+input_pdb+"\n")
         input_pymol.write("set dash_gap, 0\n")
         input_pymol.write("set dash_width, 2\n")
         input_pymol.write("preset.pretty(selection='all')\n")
-        
-        color = np.max(self.matrice_MI)/np.max(np.max(self.matrice_MI))
+
+        color = np.max(self.matrice_mi)/np.max(np.max(self.matrice_mi))
 
         color = 10*(color - color.min()) / (color.max() - color.min())
         palette = self.color_palette()
 
-        for i in range(len(self.matrice_MI)):
-            input_pymol.write("set_color color_{} =   [{} , {} , {}]\n".format(i,\
+        for i in range(len(self.matrice_mi)):
+            input_pymol.write("set_color color_{} =   [{} , {} , {}]\n"\
+                                .format(i,\
                                 palette[int(color[i])][0],\
                                 palette[int(color[i])][1],\
                                 palette[int(color[i])][2]))
-            input_pymol.write("color color_{}, resi {}\n".format(i,i+1))
-        
-            for j in range(i, len(self.matrice_MI)):
-                if self.matrice_MI[i][j] > line_threshold:
+            input_pymol.write("color color_{}, resi {}\n".format(i, i+1))
 
-                    input_pymol.write("distance {}_{},".format(i + 1,j + 1)+\
+            for j in range(i, len(self.matrice_mi)):
+                if self.matrice_mi[i][j] > line_threshold:
+
+                    input_pymol.write("distance {}_{},".format(i + 1, j + 1)+\
             "name CA and resi {}, ".format(i + 1)+ "name CA and resi {}\n".format(j + 1))
-                    input_pymol.write("hide labels, "+"{}_{}\n".format(i + 1,j + 1))
-
-                    
+                    input_pymol.write("hide labels, "+"{}_{}\n".format(i + 1, j + 1))
 
         input_pymol.close()
         os.system("pymol -p pymol_tmp.pml")
- 
 
 
-#### MAIN ####
-if __name__=="__main__":
-    
+def analysis_mutual_information():
+    """
+    Perform the anylisis by computing the mutual information and dealing with 
+    the different arguments.
+    """
     parser = argparse.ArgumentParser()
-    
-    parser.add_argument("-f", nargs = "?",  \
-     help = "output a fasta file containing the generated structural alphabet", const = "pbxplore_sequence.fasta")
-    parser.add_argument("-py", nargs = "?", \
-     help = "generate the structure network according to the MI matrix on pymol\ntake in argument the pdb file of the structure")
-    parser.add_argument("-omi", nargs = "?",\
-     help = "generate an output file containing the mutual information matrix ", const = "mi_matrix.csv")
-    
-    parser.add_argument("-hmap", help = "show the heatmap of the mutual information matrix")
-    
-    parser.add_argument("input_topology", help = "number of molecular dynamics to analyse")
-    parser.add_argument("input_trajectory", help = "number of molecular dynamics to analyse")
-    
+
+    parser.add_argument("-f", nargs="?",\
+        help="output a fasta file containing the generated structural" + \
+             " alphabet",\
+        const="pbxplore_sequence.fasta")
+
+    parser.add_argument("-py", nargs="?", \
+        help="generate the structure network according to the MI matrix" + \
+             " on pymol\ntake in argument the pdb file of the structure")
+    parser.add_argument("-omi", nargs="?",\
+        help="generate an output file containing the mutual information" + \
+             " matrix ",\
+        const="mi_matrix.csv")
+
+    parser.add_argument("-hmap",\
+        help="show the heatmap of the mutual information matrix")
+
+    parser.add_argument("input_topology",\
+        help="number of molecular dynamics to analyse")
+    parser.add_argument("input_trajectory",\
+        help="number of molecular dynamics to analyse")
+
     args = parser.parse_args()
 
-    dt = AssignationPbxplore(args.input_topology, args.input_trajectory, args)
+    pbxplore_seq_md = AssignationPbxplore(args)
 
-    stat = Statistique(dt.md_sa_seq)
+    stats_mi = Statistique(pbxplore_seq_md.md_sa_seq)
 
-    MI = stat.mutual_information()
-    print(MI)
-    mat_visual = Visualisation(MI)
-    
+    matrix_mi = stats_mi.mutual_information()
+
+    mat_visual = Visualisation(matrix_mi)
+
     if args.hmap:
         mat_visual.visualize_matrice_mi()
 
@@ -248,27 +260,31 @@ if __name__=="__main__":
         mat_visual.write_mi_csv(args)
 
     if args.py:
-        change_threshold = input("You asked for the network visualization with pymol\n" + \
-        "do you want to change the threshold for the network representation ? (y/n) :")
+        change_threshold = input(\
+                    "You asked for the network visualization with pymol\n" + \
+                    "do you want to change the threshold for the network " + \
+                    "representation ? (y/n) :")
         while change_threshold != "y" and change_threshold != "n":
-            change_threshold = input("bad input.\nDo you want to change the threshold for the network representation ? (y/n) : ")
+            change_threshold = input(\
+                    "bad input.\nDo you want to change the threshold for" + \
+                    " the network representation ? (y/n) : ")
         if change_threshold == "y":
-            threshold = max(MI.max()) + 1
-            while threshold >= max(MI.max()) or threshold <= min(MI.max()):
+            threshold = max(matrix_mi.max()) + 1
+            while threshold >= max(matrix_mi.max()) \
+               or threshold <= min(matrix_mi.max()):
                 while True:
                     try:
-                        threshold = int(input("Value of the threshold: (max value = {:.1f} ; min value = {:.1f}):".format(max(MI.max()),min(MI.min()))))
+                        threshold = int(input(\
+    "Value of the threshold: (max value = {:.1f} ; min value = {:.1f}):"\
+    .format(max(matrix_mi.max()), min(matrix_mi.min()))))
                         break
                     except ValueError:
                         print("This is not a valid number. Try again.")          
         else:
-            threshold = np.max(np.max(MI))/2
+            threshold = np.max(np.max(matrix_mi))/2
         mat_visual.show_pymol_network(args.py, threshold)
 
 
-
-
-
-
-
-
+#### MAIN ####
+if __name__ == "__main__":
+    analysis_mutual_information()
