5b409aa6ee30b258648214f6bf08c149b04020bb
m2bi Guillaume OLLITRAULT <guillaume.ollitrault@gmail.com>
2019-09-17 14:49:43 +0200

    programme tout beau

--- src/projet.py
+++ src/projet.py
@@ -1,3 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Read a molecular dynamics and assign for each frames protein blocs[1]
+The program then compue the mutual information matrix and represent the matrix
+in the form of an heatmap and/or a structure network with pymol[2]. This program
+is inspired from GSATools[3].
+
+2019 - Guillaume OLLITRAULT
+
+[1]
+Barnoud J, Santuz H, Craveur P, Joseph AP, Jallu V, de Brevern AG, Poulain P,
+PBxplore: a tool to analyze local protein structure and deformability with Protein Blocks
+PeerJ 5:e4013 https://doi.org/10.7717/peerj.4013 (2017).
+[2]
+The PyMOL Molecular Graphics System, Version 1.8.4.0 Schrödinger, LLC.
+[3]
+Pandini A, Fornili A, Fraternali F, Kleinjung J
+"GSATools: analysis of allosteric communication and functional local motions
+ using a Structural Alphabet"
+Bioinformatics 29(16):2053-2055, 2013
+"""
+
 import os
 import argparse
 import numpy as np
@@ -12,7 +35,7 @@ SA_LETTERS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",\
 
 class AssignationPbxplore(object):
     """
-    This class assign structural letters to a protein structure with pbxplore 
+    This class assign structural letters to a protein structure with pbxplore
     to each frame of a molecular dynamics. The class takes 3 arguments:
 
     - the topology of the structure
@@ -25,7 +48,7 @@ class AssignationPbxplore(object):
         self.md_sa_seq = []
         if args.f:
             file_output_fasta = open(args.f, "w")
-        for chain_name, chain in pbx.chains_from_trajectory(
+        for chain_name, chain in pbx.chains_from_trajectory(\
                                                         args.input_trajectory,\
                                                         args.input_topology):
             dihedrals = chain.get_phi_psi_angles()
@@ -39,8 +62,8 @@ class AssignationPbxplore(object):
 
 class Statistique(object):
     """
-    This class contain the differents methods to perform the statistics 
-    analysis of the set of structural letters of the structure during the 
+    This class contain the differents methods to perform the statistics
+    analysis of the set of structural letters of the structure during the
     trajectory.
     This class actually compute the mutual information.
     It takes one argument:
@@ -51,7 +74,7 @@ class Statistique(object):
 
     def matrice_p_a(self):
         """
-        Method calculate the probability for a structural letter to occur in 
+        Method calculate the probability for a structural letter to occur in
         the molecular dynamics. P(a)
         Return a matrix 1*SA_SIZE with the probability for each letters
         to occur.
@@ -61,26 +84,26 @@ class Statistique(object):
         for i in range(len(self.md_sa_seq)):
             for j in range(len(self.md_sa_seq[0])):
                 mat_probabilities[0][self.md_sa_seq[i][j]] += 1
-                
+
         return mat_probabilities/(len(self.md_sa_seq)*len(self.md_sa_seq[0]))
 
     def matrice_p_ab(self):
         """
-        Method calculate the probability for a two set of structural letters to 
-        occur one after an other during the molecular dynamics. It count the 
-        number of time a set of structural letts is seen between two frame of 
+        Method calculate the probability for a two set of structural letters to
+        occur one after an other during the molecular dynamics. It count the
+        number of time a set of structural letts is seen between two frame of
         the molecular dynamics.
-        ex: in the first frame in the position 1 the letter "a" is seen and in 
+        ex: in the first frame in the position 1 the letter "a" is seen and in
         the frame 2 at the position 1 the letter "b" is seen.
-        the method count these occurences and divide them with the number of 
+        the method count these occurences and divide them with the number of
         positions in the structure * the number of frames of the molecular
         dynamics.
-        Return a matrix of size SA_SIZE*SA_SIZE with the probability for each 
+        Return a matrix of size SA_SIZE*SA_SIZE with the probability for each
         pair of letters. P(a,b)
         """
         mat_count_occurence = pd.DataFrame(                            \
                                           np.zeros((SA_SIZE, SA_SIZE)),\
-                                          columns=SA_LETTERS, index=SA_LETTERS) 
+                                          columns=SA_LETTERS, index=SA_LETTERS)
 
         for i in range(len(self.md_sa_seq)-1):
             for j in range(len(self.md_sa_seq[0])):
@@ -92,17 +115,17 @@ class Statistique(object):
 
     def mutual_information_i(self, i, j, mat_p_a, mat_p_ab):
         """
-        Compute the mutual information for two sets of positions in the 
+        Compute the mutual information for two sets of positions in the
         structure. MI(X;Y) =  ∑ P(x;y)*log(P(x;y)/(P(x)*P(y)))
         """
         mutual_information = 0
-        for k in range(len(self.md_sa_seq)): 
+        for k in range(len(self.md_sa_seq)):
             if mat_p_ab[self.md_sa_seq[k][i]]\
                        [self.md_sa_seq[k][j]] == 0 \
             or mat_p_a[0][self.md_sa_seq[k][i]] == 0 \
-            or mat_p_a[0][self.md_sa_seq[k][j]] == 0:  
+            or mat_p_a[0][self.md_sa_seq[k][j]] == 0:
                 mutual_information += 0
-            else:               
+            else:
                 mutual_information += \
                     mat_p_ab[self.md_sa_seq[k][i]][self.md_sa_seq[k][j]]*\
            np.log2(mat_p_ab[self.md_sa_seq[k][i]][self.md_sa_seq[k][j]]/\
@@ -112,9 +135,9 @@ class Statistique(object):
 
     def mutual_information(self):
         """
-        Compute the mutual information for each pair of positions in the 
+        Compute the mutual information for each pair of positions in the
         structure.
-        return the matrix of size (number of position inf the structure)**2 
+        return the matrix of size (number of position inf the structure)**2
         with each mutual information calculate.
         """
         mutual_information = pd.DataFrame(np.zeros((len(self.md_sa_seq[0]),\
@@ -147,20 +170,20 @@ class Visualisation(object):
             for j in range(len(self.matrice_mi)):
                 file_csv.write("{},".format(self.matrice_mi[i][j]))
             file_csv.write("\n")
-        file_csv.close()       
+        file_csv.close()
     def visualize_matrice_mi(self):
         """
         Represent the mutual information matrix in form of a heatmap
         """
-        ax = plt.axes()  
-        sns.heatmap(self.matrice_mi, ax=ax)
-        ax.set_title(\
+        ax_heatmap = plt.axes()
+        sns.heatmap(self.matrice_mi, ax=ax_heatmap)
+        ax_heatmap.set_title(\
        "Heatmap of the compute mutual information for the different positions")
         plt.show()
 
     def color_palette(self):
         """
-        r,g,b numbers for the color palette to visualize with pymol 
+        r,g,b numbers for the color palette to visualize with pymol
         the important residues involved in the network.
         The colors goes from the blue to red.
         """
@@ -176,13 +199,13 @@ class Visualisation(object):
                  [1.00, 0.10, 0.00],\
                  [1.00, 0.00, 0.00]]
         return color
-  
+
     def show_pymol_network(self, input_pdb, line_threshold):
         """
-        launch pymol and read a pml file which color the résidues according 
-        their significance in the network made with the mutual information. 
+        launch pymol and read a pml file which color the résidues according
+        their significance in the network made with the mutual information.
         Put a line between the important residues (>threshold).
-        the threshold is is the maximum value of the matrix divided by 2 but 
+        the threshold is is the maximum value of the matrix divided by 2 but
         can be given by the user.
         """
         input_pymol = open("pymol_tmp.pml", "w")
@@ -217,7 +240,7 @@ class Visualisation(object):
 
 def analysis_mutual_information():
     """
-    Perform the anylisis by computing the mutual information and dealing with 
+    Perform the anylisis by computing the mutual information and dealing with
     the different arguments.
     """
     parser = argparse.ArgumentParser()
@@ -279,7 +302,7 @@ def analysis_mutual_information():
     .format(max(matrix_mi.max()), min(matrix_mi.min()))))
                         break
                     except ValueError:
-                        print("This is not a valid number. Try again.")          
+                        print("This is not a valid number. Try again.")
         else:
             threshold = np.max(np.max(matrix_mi))/2
         mat_visual.show_pymol_network(args.py, threshold)
